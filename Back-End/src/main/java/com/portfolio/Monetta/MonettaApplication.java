ce1674a75005b6f2cc127e71cddb7e6fe3d56382
SamyMonetta <samisami_monetta@hotmail.com>
2022-08-23 15:17:46 -0300

    se crea back end

---
+++ Back-End/src/main/java/com/portfolio/Monetta/MonettaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Monetta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonettaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonettaApplication.class, args);
+	}
+
+}
---
+++ Back-End/src/test/java/com/portfolio/Monetta/MonettaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Monetta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonettaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
